e74bbf90d9fc0335c0ca981d7df1e5e4bdc437bf
yatno <moch.elfitriyanto@gmail.com>
2021-01-02 19:46:49 +0700

    hasil kerja yatno

--- app/Views/info.php
+++ app/Views/info.php
@@ -10,6 +10,6 @@
 </ul>
 <p>Informasi Kegiatan Siswa Bulan Depan :</p>
 <ul>
-    <li>12 September - Ujian Tengah Semester lalala</li>
+    <li>12 September - Ujian Tengah Semester lalala HABIB ANJENG</li>
 </ul>
 <?= $this->endSection(); ?>
